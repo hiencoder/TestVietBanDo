d22df69230d1bb17d1b29809b72733114cbacda2
hiennv <hiennv@rikkeisoft.com>
2019-03-20 17:45:15 +0700

    Commit polygon

--- app/src/main/java/com/example/hiennv/vietbandodemo/PolygonActivity.java
+++ app/src/main/java/com/example/hiennv/vietbandodemo/PolygonActivity.java
@@ -1,8 +1,10 @@
 package com.example.hiennv.vietbandodemo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.vietbando.vietbandosdk.annotations.PolygonOptions;
 import com.vietbando.vietbandosdk.camera.CameraPosition;
 import com.vietbando.vietbandosdk.constants.Style;
 import com.vietbando.vietbandosdk.geometry.LatLng;
@@ -10,6 +12,9 @@ import com.vietbando.vietbandosdk.maps.MapView;
 import com.vietbando.vietbandosdk.maps.OnMapReadyCallback;
 import com.vietbando.vietbandosdk.maps.VietbandoMap;
 import com.vietbando.vietbandosdk.maps.VietbandoMapOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PolygonActivity extends BaseActivity implements OnMapReadyCallback {
     @Override
@@ -22,7 +27,7 @@ public class PolygonActivity extends BaseActivity implements OnMapReadyCallback 
         super.onCreate(savedInstanceState);
         VietbandoMapOptions options = new VietbandoMapOptions()
                 .camera(new CameraPosition.Builder()
-                        .target(new LatLng(21.969158830062938, 106.71006523224813))
+                        .target(new LatLng(10.7773973, 106.701278))
                         .zoom(15)
                         .build());
 
@@ -37,6 +42,22 @@ public class PolygonActivity extends BaseActivity implements OnMapReadyCallback 
     public void onMapReady(VietbandoMap vietbandoMap) {
         mMap = vietbandoMap;
         mMap.setStyle(Style.VIETBANDO_VT_DEFAULT);
+        drawPolygon(mMap);
+    }
+
+    /**
+     * Draw polygon
+     *
+     * @param map
+     */
+    private void drawPolygon(VietbandoMap map) {
+        List<LatLng> polygon = new ArrayList<>();
+        polygon.add(new LatLng(10.7773973, 106.701278));
+        polygon.add(new LatLng(10.7753973, 106.700178));
+        polygon.add(new LatLng(10.7753973, 106.702378));
+        map.addPolygon(new PolygonOptions()
+                .addAll(polygon)
+                .fillColor(Color.parseColor("#3bb2d0")));
     }
 
 
